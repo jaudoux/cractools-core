4d903fee5b8160d009dbe3b79c3a141020e6fbd8
Jérôme Audoux <jerome.audoux@gmail.com>
2014-10-21 11:53:48 +0200

    BUG in Chimera event parsing. The class was parsed as the score

--- lib/CracTools/SAMReader/SAMline.pm
+++ lib/CracTools/SAMReader/SAMline.pm
@@ -870,12 +870,13 @@ sub loadEvents {
                       other2 => $other2,
                     );
         } elsif($event_type eq 'chimera') {
-          my ($pos_read,$loc1,$loc2,$score) = split(':',$event_infos); 
+          my ($pos_read,$loc1,$loc2,$class,$score) = split(':',$event_infos); 
           my ($chr1,$pos1,$strand1) = expandCracLoc($loc1);
           my ($chr2,$pos2,$strand2) = expandCracLoc($loc2);
           %event_hash = ( pos => $pos_read,
                       loc1 => {chr => $chr1, pos => $pos1, strand => $strand1},
                       loc2 => {chr => $chr2, pos => $pos2, strand => $strand2},
+                      class => $class,
                       score => $score,
                     );
         } elsif($event_type eq 'Undetermined') {
